6964ea5f11822b785f518d2f29e7d85f011f1a23
laziza-xolbutayeva <lazizaxolbutayeva156@gmail.com>
2022-08-19 23:02:43 +0500

    function

---
+++ script.js
@@ -0,0 +1,101 @@
+"use strick"
+
+
+// Function
+
+
+// let a = +prompt("ihtioriy son kiriting");
+// function degree(num) {
+//     return Math.pow(num, 3);
+// }
+// alert(degree(a));
+
+//2-example
+// let a = +prompt("ixtioriy son kiriting");
+// let b = +prompt("ixtioriy son kiriting");
+
+// function add(num1, num2) {
+//     return num1 + num2;
+// }
+// alert(add(a, b));
+
+//3-example
+// let a = +prompt("ixtioriy son kiriting");
+// let b = +prompt("ixtioriy son kiriting");
+
+// function minus(num1, num2) {
+//     return num1 - num2;
+// }
+// alert(add(a, b));
+
+//4-example
+// let a = +prompt("ixtioriy son kiriting");
+// let degree = function(num) {
+//     console.log(Math.pow(num, 2), Math.pow(num, 3), Math.pow(num, 4));
+// }
+// degree(a);
+
+//5-example
+// let n = +prompt("ixtioriy son kiriting");
+// function odd(num) {
+//     for (let i = 1; i <= num; i++) {
+//         if (i % 2 == 1) console.log(i)
+//     }
+// }
+// odd(n);
+
+//6-example
+// let n = +prompt("ixtioriy son kiriting");
+// function couple(num) {
+//     for (let i = 1; i <= num; i++) {
+//         if (i % 2 == 0) console.log(i)
+//     }
+// }
+// couple(n);
+
+//7-example
+// let n = +prompt("ixtioriy son kiriting");
+// function IncrementDecrement(num) {
+//     console.log(num + 1, num - 1)
+// }
+// IncrementDecrement(n);
+
+//8-example
+// let n = +prompt("ixtioriy son kiriting");
+// function divisor(num) {
+//     for (let i = 1; i <= num; i++) {
+//         if (num % i == 0) console.log(i);
+//     }
+// }
+// divisor(n);
+
+//9-example
+// let a = +prompt("ixtioriy son kiriting");
+// let b = +prompt("ixtioriy son kiriting");
+// let c = prompt("amalni kiriting");
+// let action = function(a, b, c) {
+//     switch (c) {
+//         case "+":
+//             console.log(a + b);
+//             break;
+//         case "-":
+//             console.log(a - b);
+//             break;
+//         case "*":
+//             console.log(a * b);
+//             break;
+//         case "/":
+//             console.log(a / b);
+//             break;
+//     }
+// }
+// action(a, b, c)
+
+//10-example
+// let n = +prompt("son kiriting");
+// function table(n) {
+//     for (let i = 1; i <= 10; i++) {
+//         console.log(`${n}*${i}=${n*i}`);
+//     }
+// }
+// table(n);
